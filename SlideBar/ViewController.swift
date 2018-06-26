0c9715ae29679c9e5251b9eade7ea12419ce5bf5
Archie <archie@vinova.sg>
2018-06-26 13:47:43 +0700

    the first commit

---
+++ SlideBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlideBar
+//
+//  Created by hieu nguyen on 6/26/18.
+//  Copyright © 2018 hieu nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
